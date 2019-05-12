f20136067afd95674f600faef92b0b099238c939
jameswebdev <jameswebdev@outlook.com>
2019-05-12 14:56:10 +0100

    Space Portal web site
    Space Poral educational site site

---
+++ components/Nav.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav class="navbar navbar-expand-lg">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <nuxt-link to='home' class="nav-link" href="#">Home <span class="sr-only">(current)</span></nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to='earth' class="nav-link" href="#">Earth</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to='milkyway' class="nav-link" href="#">Milky Way</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to='ourgalaxy' class="nav-link" href="#">Our Galaxy</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to='solarsystem' class="nav-link" href="#">Solar System</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to='sun' class="nav-link" href="#">Sun</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link to='universe' class="nav-link" href="#">Universe</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Nav"
+};
+</script>
+
+<style>
+.navbar .nav-link {
+  color: cadetblue;
+  background-color: transparent;
+  font-size: 1.5rem;
+}
+
+.nav-link {
+  justify-content: space-evenly;
+  margin: auto;
+}
+
+.navbar-toggler {
+  color: white;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="default">
+    <div><h1 class="text-center" id="title">SPACE PORTAL</h1></div>
+    <div id="main_body">
+      <Nav class="col-lg-12"></Nav>
+    </div>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import Nav from "@/components/Nav";
+
+export default {
+  name: "default",
+  components: {
+    Nav
+  }
+};
+</script>
+
+<style>
+html {
+  box-sizing: border-box;
+}
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+#title {
+  font-size: 4rem;
+  font-style: italic;
+  color: cadetblue;
+  padding-bottom: 10px;
+}
+
+#default {
+  background-image: url("../assets/img/space-background.jpg");
+}
+
+#main_body {
+  display: grid;
+  grid-template-columns: 100% 1fr;
+  grid-template-rows: auto;
+}
+</style>
